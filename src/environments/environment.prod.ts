247f67016c24fc053e6bd03cf12621f3189209af
Yevheniy Potupa <y.potupa@gmail.com>
2018-01-09 17:06:17 +0200

    changed firebase account

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,17 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDSVhbTiygqYiFYP0u3HDGxqveroPV69GI",
-    authDomain: "flyhigh-f209c.firebaseapp.com",
-    databaseURL: "https://flyhigh-f209c.firebaseio.com",
-    projectId: "flyhigh-f209c",
-    storageBucket: "flyhigh-f209c.appspot.com",
-    messagingSenderId: "1051243187178"
+    apiKey: 'AIzaSyCTytzH3FVe6cEnDfsgeSpiJ58PV2M3XZE',
+    authDomain: 'flyhigh-5416b.firebaseapp.com',
+    databaseURL: 'https://flyhigh-5416b.firebaseio.com',
+    projectId: 'flyhigh-5416b',
+    storageBucket: 'flyhigh-5416b.appspot.com',
+    messagingSenderId: '665055994302'
+    // apiKey: 'AIzaSyDSVhbTiygqYiFYP0u3HDGxqveroPV69GI',
+    // authDomain: 'flyhigh-f209c.firebaseapp.com',
+    // databaseURL: 'https://flyhigh-f209c.firebaseio.com',
+    // projectId: 'flyhigh-f209c',
+    // storageBucket: 'flyhigh-f209c.appspot.com',
+    // messagingSenderId: '1051243187178'
   }
 };
