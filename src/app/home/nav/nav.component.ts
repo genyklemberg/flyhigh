8ff5d4f7a1b5208644905f73a55fc821aa8a6e6b
Yevheniy Potupa <y.potupa@gmail.com>
2018-01-19 11:28:37 +0200

    fixed nav

--- src/app/home/nav/nav.component.ts
+++ src/app/home/nav/nav.component.ts
@@ -11,42 +11,27 @@ import {ViewEncapsulation} from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  mobileNav=false;
-
   constructor(public router: Router,
               private scrollService: ScrollService) { }
 
   ngOnInit() {
+
+    window.addEventListener('scroll', function() {
+      // //Here you forgot to update the value
+      const scrollpos = window.scrollY;
+      const header = document.getElementById('header');
+
+      if (scrollpos > 10) {
+        header.classList.add('white-bg');
+      } else {
+        header.classList.remove('white-bg');
+      }
+    });
   }
 
   scrollTo(target) {
     this.scrollService.triggerScrollTo(target);
   }
-
-  // toggleNav() {
-  //   this.mobileNav=true;
-  // }
-
-    //use window.scrollY
-  var scrollpos = window.scrollY;
-  var header = document.getElementById("header");
-
-
-  window.addEventListener('scroll', function(){ 
-      //Here you forgot to update the value
-      scrollpos = window.scrollY;
-      header = document.getElementById("header");
-
-      if(scrollpos > 10){
-        header.classList.add("white-bg");
-        console.log('scroll to bottom');
-      }
-      else {
-        header.classList.remove("white-bg")
-        console.log('top of page');
-      }
-      console.log(scrollpos);
-  });
 
 }
 
