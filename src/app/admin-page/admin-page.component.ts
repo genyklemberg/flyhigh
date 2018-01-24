cd711970916c70063898030a238db8e0e5fba800
Valdemar26 <z_valdemar@ukr.net>
2018-01-24 14:31:50 +0200

    create Article tab

--- src/app/admin-page/admin-page.component.ts
+++ src/app/admin-page/admin-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ViewEncapsulation } from '@angular/core';
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { ProductService } from "../products/product.service";
+import { BlogService } from "../blog/blog.service";
 import { Router } from "@angular/router";
 import { AngularFireDatabase } from "angularfire2/database";
 
@@ -15,23 +16,27 @@ export class AdminPageComponent implements OnInit {
   categoryForm: FormGroup;
   subCategoryForm: FormGroup;
   itemsForm: FormGroup;
+  blogForm: FormGroup;
   categories;
   subcategories;
   items;
+  article;
   categoriesRef;
 
   constructor(private productService: ProductService,
+              private blogService: BlogService,
               private router: Router,
               private db: AngularFireDatabase) {
     this.categoriesRef = db.list('/products/category');
     this.categories = productService.getCategoryList();
     this.subcategories = productService.getSubCategoryList();
     this.items = productService.getProductsList();
+    this.article = blogService.getBlogList();
 
-    console.log('subcategories: ', this.subcategories);// return Observable
-                                            // {_isScalar: false, source: Observable, operator: MapOperator}
+    console.log('subcategories: ', this.subcategories);
     console.log('this.categories: ', this.categories);
     console.log('this.items: ', this.items);
+    console.log('this.article: ', this.article);
   }
 
   ngOnInit() {
@@ -50,6 +55,13 @@ export class AdminPageComponent implements OnInit {
       'title': new FormControl('', Validators.required),
       'body': new FormControl('', Validators.required),
       'item_id': new FormControl('', Validators.required)
+    });
+
+    this.blogForm = new FormGroup({
+      'body': new FormControl('', Validators.required),
+      'title': new FormControl('', Validators.required),
+      'type': new FormControl('', Validators.required),
+      'img': new FormControl('')
     })
 
   }
@@ -90,12 +102,32 @@ export class AdminPageComponent implements OnInit {
     });
   }
 
+  addArticle() {
+    const promise = new Promise((resolve, reject) => {
+      this.blogService.articleForm(
+          this.blogForm.value['body'],
+          this.blogForm.value['title'],
+          this.blogForm.value['type'],
+          this.blogForm.value['img']
+      );
+      this.blogForm.reset();
+    });
+  }
+
   deleteCategory(key: string):void {
     this.db.object(`/products/category/${key}`).remove();
   }
 
+  deleteSubcategory(key:string):void {
+    this.db.object(`products/subcategory/${key}`).remove();
+  }
+
   deleteItem(key:string):void {
     this.db.object(`/products/items/${key}`).remove();
+  }
+
+  deleteArticle(key:string):void {
+    this.db.object(`/blog/${key}`).remove();
   }
 
 }
--- src/app/products/product.ts
+++ src/app/products/product.ts
@@ -1,9 +1,8 @@
 export class IProduct {
   $key?: string;
-  title: string;
   body?: string;
-  category: string;
-  subcategory: string;
-  images?: any[];
-  item_id?: number;
+  img?: any[];
+  type?: string;
+  title: string;
+  timeStamp?: number;
 }
