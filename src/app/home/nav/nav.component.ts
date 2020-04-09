e398480d096e97e3d1a5904d6799c0fa0821949e
Yevheniy Potupa <y.potupa@gmail.com>
2020-04-09 14:57:05 +0300

    some styles fix

--- src/app/home/nav/nav.component.ts
+++ src/app/home/nav/nav.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {ScrollService} from '../../scroll.service';
 
 @Component({
@@ -8,18 +8,18 @@ import {ScrollService} from '../../scroll.service';
   styles: [],
   encapsulation: ViewEncapsulation.None
 })
-export class NavComponent implements OnInit {
+export class NavComponent implements OnInit{
 
   constructor(public router: Router,
+              public route: ActivatedRoute,
               private scrollService: ScrollService) {
   }
 
   ngOnInit() {
+    const header = document.getElementById('header');
 
     window.addEventListener('scroll', function () {
-      // //Here you forgot to update the value
       const scrollPos = window.scrollY;
-      const header = document.getElementById('header');
 
       if (scrollPos > 10) {
         header.classList.add('white-bg');
@@ -27,9 +27,19 @@ export class NavComponent implements OnInit {
         header.classList.remove('white-bg');
       }
     });
+    // this.router.events.filter(e => e instanceof NavigationEnd)
+    //   .subscribe((data: any) => {
+    //     if (this.checkRoute(data.url)) {
+    //       header.classList.add('opacity-bg');
+    //     }
+    //   });
   }
 
   scrollTo(target) {
     this.scrollService.triggerScrollTo(target);
   }
+
+  // checkRoute(url) {
+  //   return url.includes('service');
+  // }
 }
